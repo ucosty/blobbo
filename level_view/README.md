0a21c7acc54f271f12e45b4cfcd8d42f04e352c4
Matthew Costa <ucosty@gmail.com>
2017-01-07 23:47:42 +0000

    small clean up, added readme

---
+++ level_view/README.md
@@ -0,0 +1,16 @@
+# Blobbo Level Viewer
+
+### Building
+
+Run make to build. Currently requires Clang++ and SFML to build.
+
+### Controls
+
+```n``` Go to the next level
+```p``` Go to the previous level
+```f``` Back to the first level
+```q``` Quit
+
+### Extra
+
+There is both a SFML and text mode backend. Currently both run concurrently.
--- level_view/level_view.cpp
+++ level_view/level_view.cpp
@@ -13,71 +13,8 @@
 #include "SFMLFrontend.h"
 
 const char pixmapFilename[] = "img/00128.png";
-const unsigned short tile_size = 16;
 const unsigned int level_min = 128;
 const unsigned int level_max = 155;
-
-struct HexCharStruct
-{
-    unsigned char c;
-    HexCharStruct(unsigned char _c) : c(_c) { }
-};
-
-inline std::ostream& operator<<(std::ostream& o, const HexCharStruct& hs)
-{
-    return (o << std::setw(2) << std::setfill('0') << std::hex << (int)hs.c);
-}
-
-inline HexCharStruct hex(unsigned char _c)
-{
-    return HexCharStruct(_c);
-}
-
-void hexOutput(char* input, int length, int width)
-{
-    unsigned int value = 0;
-    for (int i = 0; i < length; i++)
-    {
-        std::cout << hex(input[i]) << ' ';
-        if ((i + 1) % width == 0) std::cout << std::endl;
-    }
-    std::cout << std::endl;
-}
-
-char *loadBlevResource(std::string filename)
-{
-
-    std::ifstream file;
-    file.open(filename, std::ios::ate | std::ios::binary);
-    size_t length = file.tellg();
-    char *source = new char[length];
-    file.seekg(0, std::ios::beg);
-    file.read(source, length);
-    file.close();
-
-    char *encrypted = &source[16];
-    char *unpacked = new char[640];
-
-    size_t length_packed = length - 16;
-
-    // It looks like all official blobbo maps start with the signature 0x7FFF
-    const unsigned short *signature = reinterpret_cast<unsigned short *>(&source[0]);
-    if(signature[0] != 0xFF7F && length == 640) {
-        return source;
-    }
-
-    char encode = source[15], nextencode;
-    for(int i = 0; i < length_packed; i++) {
-        nextencode = encrypted[i];
-        encrypted[i] ^= encode;
-        encode = nextencode;
-    }
-
-    Toolbox::ToolUtils::UnpackBits((unsigned char *)encrypted, (unsigned char *)unpacked, length_packed);
-
-    delete[] source;
-    return unpacked;
-}
 
 std::vector<sf::RectangleShape> tiles;
 sf::Texture textures;
